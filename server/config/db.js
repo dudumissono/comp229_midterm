ab1fe8e787fcf617e7732f939c1585f374304640
Dudu Missono <dudumissono@gmail.com>
2022-10-22 15:23:44 -0400

    trying to connect with heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  // "URI": "mongodb://localhost/books229"
+  'URI': 'mongodb+srv://emissono:V9GGlHMQBBAsJxYv@mongodbserver.b5zcdkt.mongodb.net/books229?retryWrites=true&w=majority'
 };
